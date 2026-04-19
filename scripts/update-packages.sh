3a57dbf4ebaeb3a2092819287e15aef558a36049
papatuoni <105186707+papatuoni@users.noreply.github.com>
2026-04-19 12:13:06 +0800

    Update update-packages.sh

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -136,6 +136,8 @@ if [ -d "openwrt-passwall-packages" ]; then
 	done
 	rm -rf openwrt-passwall-packages
 fi
+# turboacc
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 echo " "
 echo "=========================================="
